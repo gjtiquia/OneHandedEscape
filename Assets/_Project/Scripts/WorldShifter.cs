4ca5f2f73ee7e0dfc1666cce30c08c8b13138205
GJ Tiquia <gershom.tiquia@gmail.com>
2023-09-24 20:54:01 +0800

    feat: added trail renderer to player

--- Assets/_Project/Scripts/WorldShifter.cs
+++ Assets/_Project/Scripts/WorldShifter.cs
@@ -1,17 +1,24 @@
 using UnityEngine;
+using UnityEngine.Events;
 
 namespace Project
 {
     public class WorldShifter : MonoBehaviour
     {
+        [Header("Settings")]
         [SerializeField] private float _shiftAmount;
         [SerializeField] private Transform _shiftTransform;
+
+        [Header("Events")]
+        [SerializeField] private UnityEvent _onWorldShifted;
 
         public void ShiftLeft()
         {
             Vector3 currentPosition = _shiftTransform.position;
             currentPosition.x += _shiftAmount;
             _shiftTransform.position = currentPosition;
+
+            AfterShifting();
         }
 
         public void ShiftRight()
@@ -19,6 +26,8 @@ namespace Project
             Vector3 currentPosition = _shiftTransform.position;
             currentPosition.x -= _shiftAmount;
             _shiftTransform.position = currentPosition;
+
+            AfterShifting();
         }
 
         public void ShiftDown()
@@ -26,6 +35,8 @@ namespace Project
             Vector3 currentPosition = _shiftTransform.position;
             currentPosition.y += _shiftAmount;
             _shiftTransform.position = currentPosition;
+
+            AfterShifting();
         }
 
         public void ShiftUp()
@@ -33,6 +44,13 @@ namespace Project
             Vector3 currentPosition = _shiftTransform.position;
             currentPosition.y -= _shiftAmount;
             _shiftTransform.position = currentPosition;
+
+            AfterShifting();
+        }
+
+        private void AfterShifting()
+        {
+            _onWorldShifted?.Invoke();
         }
     }
 }
