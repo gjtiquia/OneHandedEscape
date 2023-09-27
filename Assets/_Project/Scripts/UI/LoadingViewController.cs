83e1acaacd5107796e7c6b5f51ef9c76703ecce3
GJ Tiquia <gershom.tiquia@gmail.com>
2023-09-27 10:31:50 +0800

    chore: imported DOTween

--- Assets/_Project/Scripts/UI/LoadingViewController.cs
+++ Assets/_Project/Scripts/UI/LoadingViewController.cs
@@ -3,11 +3,16 @@ using UnityEngine.Events;
 using UnityEngine.UI;
 using UnityAssert = UnityEngine.Assertions.Assert;
 using TMPro;
+using DG.Tweening;
 
 namespace Project.UI
 {
     public class LoadingViewController : MonoBehaviour
     {
+        [Header("Settings")]
+        [SerializeField] private float _fadeInDuration;
+
+        [Header("References")]
         [SerializeField] private Image _background;
         [SerializeField] private TextMeshProUGUI _loadingText;
 
@@ -19,8 +24,14 @@ namespace Project.UI
 
         private void Start()
         {
-            // TODO : Set all as transparent
-            // TODO : Fade in the colors with DOTween   
+            Color originalBackgroundColor = _background.color;
+            _background.color = new Color(0, 0, 0, 0);
+
+            Color originalLoadingTextColor = _loadingText.color;
+            _loadingText.color = new Color(0, 0, 0, 0);
+
+            _background.DOColor(originalBackgroundColor, _fadeInDuration);
+            _loadingText.DOColor(originalLoadingTextColor, _fadeInDuration);
         }
     }
 }
